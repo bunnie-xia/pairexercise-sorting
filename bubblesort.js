a07a4d944f3dedbf5f36e75e59db91ddc4141987
Carmen <xiawenwu22@gmail.com>
2020-04-07 13:18:29 -0400

    finish bubble sort

--- bubblesort.js
+++ bubblesort.js
@@ -1,3 +1,29 @@
 function bubbleSort(array) {
+  let counter = 0;
+  let swap = true;
+
+  while (swap) {
+    swap = false;
+
+    for (let i = 0; i < (array.length - counter); i++) {
+      if (array[i] > array[i+1]) {
+        arr = swaped(array,i,i+1);
+        swap = true;
+      }
+    }
+    counter++;
+
+  }
+
   return array;
 }
+
+function swaped(arr,leftIndex,rightIndex) {
+  const temp1 = arr[leftIndex];
+  arr[leftIndex] = arr[rightIndex];
+  arr[rightIndex] = temp1;
+  return arr;
+}
+
+
+console.log(bubbleSort([1,0,3,2,5,4]));
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -2,4 +2,15 @@ describe('Bubble Sort', function () {
   it('handles an empty array', function () {
     expect(bubbleSort([])).toEqual([]);
   });
+
+  it('sorts an array with elements', function(){
+    expect( bubbleSort([1,0,3,2,5,4])).toEqual( [0,1,2,3,4,5] );
+    spyOn(window, 'swap').and.callThrough();
+    bubbleSort([1,0,3,2,5,4]);
+    expect( swap.calls.count()).toEqual(3);
+  });
+
+  it('sorts a backwards array', function(){
+    expect( bubbleSort([5,4,3,2,1])).toEqual( [1,2,3,4,5]);
+  });
 });
